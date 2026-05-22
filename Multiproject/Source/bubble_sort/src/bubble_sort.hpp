6aa75109f2b4be1905f348470036d029b0aa498f
7n1KeRs <nikitadozmolin@gmail.com>
2026-05-22 20:30:10 +0300

    Second part of mario game

--- Multiproject/Source/bubble_sort/src/bubble_sort.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef BUBBLE_SORT_HPP
-#define BUBBLE_SORT_HPP
-
-namespace dnv {
-	void sort(int* const arr, const int size);
-}
-
-#endif
--- Multiproject/run_tests.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-chcp 65001
-ctest -j 4 --output-on-failure
