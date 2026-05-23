64a80ee3108404a209cacbd262065706c59474ef
7n1KeRs <nikitadozmolin@gmail.com>
2026-05-23 16:20:55 +0300

    Third part of mario game: Little changes

---
+++ 260522_cpp_mario_OOP/GameObject.cpp
@@ -0,0 +1,47 @@
+#include "GameObject.hpp"
+
+#include <cmath>
+
+using dnv::GameObject;
+
+GameObject::GameObject()
+{
+	x = 0;
+	y = 0;
+	height = 0;
+	width = 0;
+	kind = ' ';
+}
+
+bool GameObject::check_collision(const GameObject& obj)
+{
+	return (x +  width) > obj.x
+	&& x < (obj.x + obj.width)
+	&& (y +  height) > obj.y 
+	&& y < (obj.y + obj.height);
+}
+
+float GameObject::get_height() const { return height; }
+char GameObject::get_kind() const { return kind; }
+float GameObject::get_width() const { return width; }
+float GameObject::get_x() const { return x; }
+float GameObject::get_y() const { return y; }
+
+void GameObject::init_object(
+	float init_x, float init_y, 
+	float init_width, float init_height, 
+	char init_kind
+) {
+	set_object_pos(init_x, init_y);
+	width = init_width;
+	height = init_height;
+	kind = init_kind;
+}
+	
+void GameObject::set_kind(char new_kind) { kind = new_kind; }
+void GameObject::set_object_pos(float obj_pos_x, float obj_pos_y)
+{
+	x = obj_pos_x;
+	y = obj_pos_y;
+}
+void GameObject::set_x(float new_x) { x = new_x; }
---
+++ 260522_cpp_mario_OOP/Map.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "GameObject.hpp"
+#include "Sprite.hpp"
+
+namespace dnv {
+	class Map
+	{
+		private:
+			static const int MAP_HEIGHT = 28;
+			static const int MAP_WIDTH = 80;
+			char map[MAP_HEIGHT][MAP_WIDTH + 1];
+			
+		public:
+			void add_object_on_map(const GameObject& obj);
+			void clear_map();		
+			float get_map_height() const;		
+			bool object_within_map(int x, int y);	
+			void scroll_map(
+				float dx, 
+				GameObject& player, 
+				GameObject*& blocks, int blocks_count, 
+				Sprite*& sprites, int& sprites_count
+			);
+			void show_map();
+			void show_score(int score);
+	};
+}
